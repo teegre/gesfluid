ebd3b3944f0307fb6c076278b2d51b1aa19cf89b
Stéphane MEYER <tigerlost@me.com>
2023-01-13 06:44:29 +0100

    app: HomeController: IsGranted → denyAccessUnlessGranted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+      $this->denyAccessUnlessGranted(new Expression(
+        'is_authenticated()'
+      ));
+      
+      return $this->render('home/index.html.twig');
     }
 }
