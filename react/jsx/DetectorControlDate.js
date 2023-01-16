645be456dcda38318b38ca725104d20f075b2be6
Stéphane MEYER <stm3y3r@icloud.com>
2023-01-16 14:38:51 +0100

    app: react: added DetectorControlDate component

---
+++ react/jsx/DetectorControlDate.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const DetectorControlDate = (props) => {
+
+  const controlDate = new Date(props.data.controlDate).toLocaleDateString("fr-fr")
+
+  return (
+    <div className="form-floating m-2">
+      <input
+        type="text"
+        readOnly
+        className="border border-0 form-control form-control-sm"
+        id="detectorControlDate"
+        value={controlDate}
+      />
+      <label htmlFor="detectorControlDate" className="small fw-bold text-success">
+        <i className="fas fa-clipboard-check"></i> Date de contrôle
+      </label>
+    </div>
+  )
+}
+
+export default DetectorControlDate;
