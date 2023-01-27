73cf1950dcd62298b73cfdace81bbad4d2cb2934
Stéphane MEYER <stm3y3r@icloud.com>
2023-01-27 09:51:06 +0100

    app: Admin dashboard: simplified intervention display

--- src/Controller/Admin/InterventionCrudController.php
+++ src/Controller/Admin/InterventionCrudController.php
@@ -4,11 +4,22 @@ namespace App\Controller\Admin;
 
 use App\Entity\Intervention;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class InterventionCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Intervention::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+      return [
+        'date',
+        AssociationField::new('equipment')->renderAsNativeWidget(),
+        AssociationField::new('user')->renderAsNativeWidget(),
+        'pdfPath',
+      ];
     }
 }
