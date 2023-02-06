98c88531e6aa5e5a4dad49d16696ca8c250ae31d
Stéphane MEYER <stm3y3r@icloud.com>
2023-02-06 14:27:28 +0100

    app: Admin Dashboard: pdf path → link

--- src/Controller/Admin/InterventionCrudController.php
+++ src/Controller/Admin/InterventionCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Intervention;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 
 class InterventionCrudController extends AbstractCrudController
 {
@@ -19,7 +20,7 @@ class InterventionCrudController extends AbstractCrudController
         'date',
         AssociationField::new('equipment')->renderAsNativeWidget(),
         AssociationField::new('user')->renderAsNativeWidget(),
-        'pdfPath',
+        UrlField::new('pdfPath'),
       ];
     }
 }
