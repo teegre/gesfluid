946fa2504064936d13d1053e6d101d64b510ce78
Stéphane MEYER <stm3y3r@icloud.com>
2023-02-07 13:53:44 +0100

    app: HomeController: indentation...

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-      $this->denyAccessUnlessGranted(new Expression(
-        'is_authenticated()'
-      ));
+  #[Route('/', name: 'app_home')]
+  public function index(): Response
+  {
+    $this->denyAccessUnlessGranted(new Expression(
+      'is_authenticated()'
+    ));
       
-      return $this->render('home/index.html.twig');
-    }
+    return $this->render('home/index.html.twig');
+  }
 }
