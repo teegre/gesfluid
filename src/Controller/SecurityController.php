1798ef88dce9d5c7a212f3a40f42aaa687666ab1
Stéphane MEYER <stm3y3r@icloud.com>
2023-02-07 09:04:17 +0100

    app: SecurityController: redirect admin to dashboard on login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,21 +12,22 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
+      if ($this->isGranted('ROLE_ADMIN')) {
+        return $this->redirectToRoute('admin');
+      }
+      return $this->redirectToRoute('app_home');
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+      // get the login error if there is one
+      $error = $authenticationUtils->getLastAuthenticationError();
+      // last username entered by the user
+      $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+      return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+      throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
